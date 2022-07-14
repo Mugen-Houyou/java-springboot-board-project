14813ce765a21a6c8737478d66c76c3a78a94c2c
Mugen-Houyou <a4r2@daum.net>
2022-07-14 14:44:07 +0900

    #6 - Springboot Initialization
    Initialized the Springboot project
    * Spring Web
    * Actuator
    * DevTools
    * Lombok

---
+++ Java-Springboot-Board-Project/src/test/java/com/cf/myboardtest/board_project/JavaSpringbootBoardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cf.myboardtest.board_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbootBoardProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
